c1169ce480aff8e01af48f45bccf38414d1e0c2d
audreyfogle <FogleA10@gmail.com>
2022-07-16 14:58:10 -0400

    created header and footer

---
+++ src/components/header/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <header>
+    <h1>
+        <a href="/">Audrey Fogle</a>
+    </h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="#Resume">Resume</a>
+            </li>
+            <li>
+                <a href="#contact-me">Contact Me</a>
+            </li>
+            <li>
+                <a href="#work">Work</a>
+            </li>
+            <li>
+                <a href="#About-Me">About Me</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+  )
+}
+
+export default Header;
